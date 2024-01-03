c214a6e5e34d8e0c4829f1613f4ba94e66b2bdad
Ali Ashouri <109386704+aagiser@users.noreply.github.com>
2024-01-03 13:40:24 +0330

    Delete TA_Polynomial_11.m

--- TA_Polynomial_11.m
+++
@@ -1,8 +0,0 @@
-clc; clear; close all; pause(0.5)
-
-% the numerator of z(s) :
-n = conv(conv([2 0 -1],[2 0 -1]),[1 0 0 0 3]) ;
-% the denominator of z(s) :
-d = [12 0 0 0 -10 -5 2] ;
-
-[r,p,k] = residue(n,d) ;
