89c7a6a6731e28c298e305ca08d9d39a2f9a5696
Ali Ashouri <109386704+aagiser@users.noreply.github.com>
2024-01-04 12:13:50 +0330

    Delete TA_Polynomial_8.m

--- TA_Polynomial_8.m
+++
@@ -1,8 +0,0 @@
-clc; clear; close all; pause(0.5)
-
-fun = @(x) [sin(4.*pi.*x(1).*x(2)) - x(1) + 2.*x(2) , ...
-    ((4.*pi-1)/(4.*pi)).*(exp(2.*x(1))-exp(1)) ...
-    + 4.*exp(1).*x(2).^2 - 2.*exp(1).*x(1)] ;
- x0 = [0.5 0] ;
- % roots:
- r = fsolve(fun,x0)
